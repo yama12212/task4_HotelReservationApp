c7c0f575d6298f7b66a17efe831bbdada8a2da13
yama12212 <t.yamanaka.sd@gmail.com>
2022-04-25 18:16:00 +0900

    [edit]sign_upフォーム編集
    userのnameを登録させる機能追加の他、signinフォームを編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_144443) do
+ActiveRecord::Schema.define(version: 2022_04_25_080449) do
 
   create_table "keywords", force: :cascade do |t|
     t.string "keyword"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 2022_04_22_144443) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
-    t.text "introduce", null: false
-    t.string "image_name", null: false
+    t.text "introduce"
+    t.string "image_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
