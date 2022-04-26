de3320ed7549e9cab4c42b446d2a71fa6cee273d
yama12212 <t.yamanaka.sd@gmail.com>
2022-04-26 19:06:22 +0900

    [hotfix]キーワード検索できない
    search_form_forに実装し直したところ、keyword検索がうまくいかなくなったので、バックアップとして現在までの進捗をadd

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,19 @@
 class SearchesController < ApplicationController
+  before_action :set_area, only: [:area]
+
   def area
-    @q = Room.ransack(params[:q])
-    @rooms = @q.result
+    @rooms = @area.result
     @count = @rooms.count
   end
 
   def keyword
+   
   end
+
+  private
+
+  def set_area
+    @area = Room.ransack(params[:q])
+  end
+
 end
