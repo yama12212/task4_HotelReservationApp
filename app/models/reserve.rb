454cb87834b00f085703a3765731f9fd23c562f1
yama12212 <t.yamanaka.sd@gmail.com>
2022-04-29 07:59:47 +0900

    [edit]reservesバリデーション設定
    バリデーションエラーメッセージを表示させるため、reservesのバリデーションを設定

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,5 +1,10 @@
 class Reserve < ApplicationRecord
   belongs_to :rooms
-
   belongs_to :users
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  valiadtes :number_of_people, presence: true
+  validates :room_id, presence: true
+  validates :user_id, presence: true
 end
