b6b4376d5d99fba62a8e30dcf10b76f6b901fce0
yama12212 <t.yamanaka.sd@gmail.com>
2022-04-29 07:56:06 +0900

    [edit]roomsバリデーション設定
    バリデーションエラーメッセージを表示させるため、roomsのバリデーションを設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,12 @@
 class Room < ApplicationRecord
   has_many :rooms_keywords_searches
   has_many :keywords, through: :rooms_keywords_searches
-
   has_many :reverses
-
   belongs_to :user
+
+  validates :name, presence: true
+  validates :single_rate, presence: true
+  validates :address, presence: true
+  validates :image_name, presence: true
+  validates :user_id, presence: true
 end
