8c41a1d95fe529f07643819ed9f1e97cc58a3768
yama12212 <t.yamanaka.sd@gmail.com>
2022-04-29 18:13:26 +0900

    [edit]room詳細部分編集
    rooms/showアクションのroom詳細情報部分のレイアウトを編集

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -2,4 +2,13 @@ class ReservationController < ApplicationController
   def new
     @reserve = Reserve.new
   end
+
+  def create
+  end
+
+  private
+
+  def reserve_params
+    params.require(:reserve).permit(:start_date, :end_date, :number_of_people, :room_id, :user_id)
+  end
 end
