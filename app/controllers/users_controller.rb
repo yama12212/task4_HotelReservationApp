3b8e0cbaf26c888d0bf5e4ea8d97e4a14a7eb16a
yama12212 <t.yamanaka.sd@gmail.com>
2022-05-03 22:13:35 +0900

    [edit]プロフィール編集画面編集
    [WIP]プロフィールの編集画面を編集。フォーム入力およびcreateアクション未実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   end
 
   def profile
-    
+    @users = User.find_by(id: current_user)
+  end
+
+  def update
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   }
   get 'users/account' => 'users#account'
   get 'users/profile' => 'users#profile'
+  patch 'users/:id' => 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
